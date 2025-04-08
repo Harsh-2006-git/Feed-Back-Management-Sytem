aef9b897d95bc3184c1559b68bea12af3c58a27a
Harsh Manmode <24it10ha60@mitsgwl.ac.in>
2025-04-08 11:41:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,10 @@ app.get("/", (req, res) => {
 
 async function main() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/feedback");
+    mongoose.connect(process.env.MONGODB_URI, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+});
     console.log("✅ Database connected successfully");
   } catch (err) {
     console.error("❌ Database connection failed:", err);
